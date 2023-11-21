c78637dfb94d1693e7059f5e1f536e35a1297afd
blackdot88 <thiengiathai@gmail.com>
2023-11-22 00:05:27 +1100

    Add calculation service on the web when calling the URL

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require("express");
 var app = express();
 var port = process.env.port || 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Addition endpoint
+app.get("/add/:num1/:num2", (req, res) => {
+  const num1 = parseFloat(req.params.num1);
+  const num2 = parseFloat(req.params.num2);
+  const result = num1 + num2;
+  res.send(`The sum of ${num1} and ${num2} is: ${result}`);
+});
+
 app.listen(port, () => {
   console.log("App listening to: " + port);
 });
